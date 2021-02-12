664caa0829670fbb94f59d782bbe7ffc8be22363
Nichkov Roman <nichkovroman@gmail.com>
2021-02-12 14:55:26 +0300

    footer js refactoring

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,17 +1,13 @@
 const footer = () => {
     const footerTitle = document.querySelectorAll('.footer__col-title');
-    const footerColtems = document.querySelectorAll('.footer__col-items');
-    // const colBlock = docuement.querySelector('.footer__col');
     footerTitle.forEach(item => {
         item.addEventListener('click', () =>{
-            
             item.classList.toggle('active');
             if(!item.nextElementSibling.classList.contains('active')){
                 item.nextElementSibling.classList.add('active');
                 item.nextElementSibling.style.height = 'auto';
 
                 let height = item.nextElementSibling.clientHeight + 'px';
-
                 item.nextElementSibling.style.height = '0px';
 
                 setTimeout(() =>{
@@ -20,17 +16,12 @@ const footer = () => {
             }
             else{
                 item.nextElementSibling.style.height = '0px';
-
                 item.nextElementSibling.addEventListener('transitionend', ()  =>{
                     item.nextElementSibling.classList.remove('active');
                 }, {
                     once: true
                 });
             }
-
-            
-            // item.closest('.footer__col').querySelector('.footer__col-items').classList.remove('active');
-
         });
     })
 }
