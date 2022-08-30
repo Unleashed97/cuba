f93829b812385f00e0b8fc836b39c865dd5fa9ce
Nichkov Roman <nichkovroman@gmail.com>
2022-08-30 22:52:26 +0300

    slider fix, container fix, cadillac decor added

--- src/js/custom-slider.js
+++ src/js/custom-slider.js
@@ -1,22 +1,14 @@
 const swiper = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
-    loop: false,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+    loop: true,
+    slidesPerView: 1,
   
     // Navigation arrows
     navigation: {
       nextEl: '.slider__btn-next',
       prevEl: '.slider__btn-prev',
     },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+
 
 });
